562a3ddb477c1e8b9e5011d43625b647da49753e
ArchibaldTK <archibaldkhojane@gmail.com>
2023-09-05 09:34:51 -0700

    More Classes

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import sys
+
+
+def init_board(n):
+    """Initialize an `n`x`n` sized chessboard with 0's."""
+    board = []
+    [board.append([]) for i in range(n)]
+    [row.append(' ') for i in range(n) for row in board]
+    return (board)
+
+
+def board_deepcopy(board):
+    """Return a deepcopy of a chessboard."""
+    if isinstance(board, list):
+        return list(map(board_deepcopy, board))
+    return (board)
+
+
+def get_solution(board):
+    """Return the list of lists representation of a solved chessboard."""
+    solution = []
+    for r in range(len(board)):
+        for c in range(len(board)):
+            if board[r][c] == "Q":
+                solution.append([r, c])
+                break
+    return (solution)
