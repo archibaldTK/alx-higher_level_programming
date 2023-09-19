8274d4fc4cacd546b3ce7fc2a11799d3caf904bf
ArchibaldTK <archibaldkhojane@gmail.com>
2023-09-19 08:50:49 -0700

    Almost a Circle

---
+++ 0x0C-python-almost_a_circle/README.md
@@ -0,0 +1,100 @@
+// 0x0C. Python - Almost a circle
+
+
+0. If it's not tested it doesn't work
+mandatory
+
+All your files, classes and methods must be unit tested and be PEP 8 validated. 
+
+1. Base class
+mandatory
+
+Write the first class Base
+
+2. First Rectangle
+mandatory
+
+Write the class Rectangle that inherits from Base
+
+3. Validate attributes
+mandatory
+
+Update the class Rectangle by adding validation of all setter methods and instantiation (id excluded)
+
+4. Area first
+mandatory
+
+Update the class Rectangle by adding the public method def area(self)
+
+5. Display #0
+mandatory
+
+Update the class Rectangle by adding the public method def display(self)
+6. __str__
+mandatory
+
+Update the class Rectangle by overriding the __str__
+
+7. Display #1
+mandatory
+
+Update the class Rectangle by improving the public method
+
+8. Update #0
+mandatory
+
+Update the class Rectangle by adding the public method
+
+9. Update #1
+mandatory
+
+Update the class Rectangle by updating the public method
+
+10. And now, the Square!
+mandatory
+
+Write the class Square that inherits from Rectangle
+
+11. Square size
+mandatory
+
+Update the class Square by adding the public getter and setter size
+
+12. Square update
+mandatory
+
+Update the class Square by adding the public method
+
+13. Rectangle instance to dictionary representation
+mandatory
+
+Update the class Rectangle by adding the public method
+
+14. Square instance to dictionary representation
+mandatory
+
+Update the class Square by adding the public method
+
+15. Dictionary to JSON string
+mandatory
+
+JSON is one of the standard formats for sharing data representation
+
+16. JSON string to file
+mandatory
+
+Update the class Base by adding the class method
+
+17. JSON string to dictionary
+mandatory
+
+Update the class Base by adding the static method
+18. Dictionary to Instance
+mandatory
+
+Update the class Base by adding the class method
+
+19. File to instances
+mandatory
+
+Update the class Base by adding the class method
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python3
+"""Create a rectangle class."""
+from base import Base
+
+
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Setter nad getter the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Settter and getter of the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """get the y coordinate of the Rectangle and set them."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the instance."""
+        return self.width * self.height
+
+    def display(self):
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def update(self, *args, **kwargs):
+        """Update the Rectangle with kwargs.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for n, v in kwargs.items():
+                if n == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = val
+                elif n == "width":
+                    self.width = val
+                elif n == "height":
+                    self.height = val
+                elif n == "x":
+                    self.x = val
+                elif k == "y":
+                    self.y = val
+
+    def to_dictionary(self):
+        """Dictionary representation of the instance."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Defines the print str of the instance"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
